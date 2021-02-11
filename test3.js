e111908370cf18e6157bd05cb004db836c1c445c
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2021-02-11 18:36:12 +0100

    generic

--- test3.js
+++
@@ -1,9 +0,0 @@
-function doStuff(params) {
-    console.log('doing stuff...');
-    console.log(params.stuff);
-    console.log(params.name);
-}
-
-const stuff = "lallalaa";
-const name = 'pera';
-doStuff({stuff, name});
